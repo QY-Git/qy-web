ab2c129aee28a71233545f4c9664cf9933b04b31
Young <838040356@qq.com>
2019-07-02 14:49:31 +0800

    1.升级更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,5 @@
 <template>
     <div class="home">
-        <img
-            alt="Vue logo"
-            src="../assets/logo.png"
-        >
         <div class="hello">
             <button class="bg-blue hover:bg-blue-light text-white font-bold py-2 px-4 border-b-4 border-blue-dark hover:border-blue rounded">
                 Button
